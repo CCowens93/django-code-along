74a71d7eefd3871758c1774f0b6ccb94416213e0
CCowens93 <cassandracowens93@gmail.com>
2019-11-21 12:18:49 -0500

    models created

--- tunr_project/tunr_app/models.py
+++ tunr_project/tunr_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class  Artist(models.Model):
+    name = models.CharField(max_length=100, default='n/a')
+    phot_url = models.CharField(max_length=100, default='n/a')
+    nationality = models.CharField(max_length=100, default='n/a')
+
+    def __str__(self):
+        return self.name
+
+class Song(models.Model):
+    title = models.CharField(max_length=255, default = 'n/a')
+    album = models.CharField(max_length=255, default = 'n/a')
+    preview_url = models.TextField(default = 'n/a')
+    artist = models.ForeignKey(Artist, on_delete = models.CASCADE, related_name = "songs")
+    # CASCADE deletes child elements if parent is erased i.e. if an artist is deleted, so are their songs
+
+    def __str__(self):
+        return self.title
